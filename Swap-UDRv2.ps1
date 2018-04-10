cc36b1452e8ad7f001fdeab7ec78462df425ea4c
Michael Cross <mcross@go-planet.com>
2018-04-10 10:39:26 -0500

    Adding V2
    Version 2 goes through all Route Tables in the Subscription and also through any Subscriptions peered with the VNET(s) that the NVAs are in.

--- Swap-UDRv2.ps1
+++ Swap-UDRv2.ps1
@@ -46,9 +46,14 @@ param(
     [string]$NVAResourceGroup
 )
 
+#$PrimaryNVAName = "TestingUDRVM"
+#$SecondaryNVAName = "TestingUDRVM2"
+#$NVAResourceGroup = "Testing"
+
 Write-Verbose "Failover from $PrimaryNVAName to $SecondaryNVAName starting at $(Get-Date)"
 
 # Get VMs from provided information
+Write-Verbose "Gathering NVA info"
 $PrimaryNVA = Get-AzureRMVM -ResourceGroup $NVAResourceGroup -Name $PrimaryNVAName
 $SecondaryNVA = Get-AzureRMVM -ResourceGroup $NVAResourceGroup -Name $SecondaryNVAName
 
@@ -64,7 +69,10 @@ else{
 # Initialize loop variables
 $PrimaryNVANIC = $null
 $SecondaryNVANIC = $null
-$RouteTable = $null
+$RouteTables = @()
+$NICSets = @()
+$Subs = @((Get-AzureRmContext).Subscription.Id)
+$Peerings = @()
 
 # Begin looping through NICs to swap routing
 For ($i=0;$i -lt $NICLimit;$i++) {
@@ -74,20 +82,42 @@ For ($i=0;$i -lt $NICLimit;$i++) {
     $PrimaryNVAIP = $PrimaryNVANIC.IpConfigurations[0].PrivateIpAddress
     $SecondaryNVAIP = $SecondaryNVANIC.IpConfigurations[0].PrivateIpAddress
 
+    # Add IPs from both FWs to NICSet
+    $tmpNICSet = New-Object System.Object
+    $tmpNICSet | Add-Member -MemberType NoteProperty -Name PrimaryNVAIP -Value $PrimaryNVAIP
+    $tmpNICSet | Add-Member -MemberType NoteProperty -Name SecondaryNVAIP -Value $SecondaryNVAIP
+
     # Get VNet and Subnets
     $Psubnetsplit = ($PrimaryNVANIC.IpConfigurations[0].subnet.Id).Split("/")
     $Ssubnetsplit = ($SecondaryNVANIC.IpConfigurations[0].subnet.Id).Split("/")
     $VirtualNetwork = Get-AzureRmVirtualNetwork -Name $Psubnetsplit[-3] -ResourceGroupName $Psubnetsplit[4]
+    $Peerings = $VirtualNetwork.VirtualNetworkPeerings
     $PrimaryNVASubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $VirtualNetwork  -Name $Psubnetsplit[-1]
     $SecondaryNVASubnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $VirtualNetwork  -Name $Ssubnetsplit[-1]
     
     # Confirm Primary and Secondary NICs are in the same subnet
     If ($PrimaryNVASubnet.Id -eq $SecondaryNVASubnet.Id) {
-        Write-Verbose $PrimaryNVASubnet.Name
-        # Get Route Table information
-        $RTSplit = $PrimaryNVASubnet.RouteTable.id.Split("/")
-        $RouteTable = Get-AzureRmRouteTable -ResourceGroupName $RTSplit[4] -Name $RTSplit[-1]
-        # Check all Routes and update as appropriate
+        # Store NICSet and Peering Subscription info
+        $NICSets += $tmpNICSet
+        foreach ($Peer in $Peerings){
+            $peerSub = ($Peer.RemoteVirtualNetwork.Id.Split("/"))[2]
+            If ($Subs -notcontains $peerSub) {
+                $Subs += $peerSub
+            }
+        }
+    }
+    else {
+        Write-Verbose "Subnet mismatch, moving to next set of NICs"
+        continue
+    }
+}
+
+# Iterate through each discovered Subscription and update all route tables
+foreach ($Sub in $Subs){
+    Select-AzureRmSubscription -Subscription $Sub | Out-Null
+    Write-Verbose "Updating Route Tables in $((Get-AzureRMContext).Subscription.Name) Subscription"
+    $RouteTables = Get-AzureRmRouteTable
+    foreach ($RouteTable in $RouteTables) {
         Write-Verbose "$($RouteTable.Name) is being updated:"
         Foreach ($Route in $($RouteTable.Routes)){
             # Skipping any /32 routes
@@ -95,12 +125,17 @@ For ($i=0;$i -lt $NICLimit;$i++) {
                 Write-Verbose "Skipping /32 Route: $($Route.Name)"
                 continue
             }
-            # Confirming we are only changing routes that are currently routing through the Primary Firewall
-            elseif ($Route.NextHopIpAddress -eq $PrimaryNVAIP) {
-                if ($PSCmdlet.ShouldProcess($Route.Name,"Update")){
-                    Set-AzureRmRouteConfig -RouteTable $RouteTable -Name $Route.Name -AddressPrefix $Route.AddressPrefix -NextHopType $Route.NextHopType -NextHopIpAddress $SecondaryNVAIP | Out-Null
+            # Confirming we are only changing routes that are currently routing through one of the Primary Firewall's IPs
+            else {
+                foreach ($NICSet in $NICSets) {
+                    if ($Route.NextHopIpAddress -eq $NICSet.PrimaryNVAIP) {
+                        if ($PSCmdlet.ShouldProcess($Route.Name,"Update")){
+                            Set-AzureRmRouteConfig -RouteTable $RouteTable -Name $Route.Name -AddressPrefix $Route.AddressPrefix -NextHopType $Route.NextHopType -NextHopIpAddress $NICSet.SecondaryNVAIP | Out-Null
+                        }
+                        Write-Verbose "$($Route.Name) Route NextHopIP switched to $($NICSet.SecondaryNVAIP)"
+                        break
+                    }
                 }
-                Write-Verbose "$($Route.Name) Route NextHopIP switched to $SecondaryNVAIP"
             }
         }
         # Updating the Route Table with the changed routes
@@ -108,10 +143,6 @@ For ($i=0;$i -lt $NICLimit;$i++) {
             Set-AzureRmRouteTable -RouteTable $RouteTable | Out-Null
         }
         Write-Verbose "$($RouteTable.Name) Update Complete"
-    }
-    else {
-        Write-Verbose "Subnet/Route Table mismatch, moving to next set of NICs"
-        continue
     }
 }
 Write-Verbose "Failover from $PrimaryNVAName to $SecondaryNVAName completed at $(Get-Date)"
